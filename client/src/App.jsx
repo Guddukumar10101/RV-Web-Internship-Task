3801a830233d24744895c3ca0de96f67fc2766c9
Guddu kumar <155356330+Guddukumar10101@users.noreply.github.com>
2025-07-12 12:22:04 +0530

    Frontend Initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline bg-gray-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
